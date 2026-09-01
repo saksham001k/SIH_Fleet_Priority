1b15cc97fed4c91f40126f9768cc06d2e978f176
Kanishka Bhatt <kanishkabhatt93@gmail.com>
2026-09-01 20:13:36 +0530

    Expose enhanced 3D build in the dashboard UI

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,4 +1,4 @@
-import { DigitalTwin } from './digital-twin.js';
+import { DigitalTwin } from './digital-twin.js?v=3d-assets-r4';
 
 /* App shell: fetch a run, play it back, keep the panel in sync.
  *
